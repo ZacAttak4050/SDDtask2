eeb6f2b8d508fecb07b89a6aec8397f9a6b63bf6
Sheeplet1 <42196548+Sheeplet1@users.noreply.github.com>
2018-08-09 08:37:49 +1000

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
+#config.py
+import pygame, os, sys, random, math, pickle
+from pygame.locals import *
+
+# Import Modules
+import pygame, sys, time
+from pygame.locals import *
+
+from tilemap import *
+
+# Clock
+mainclock = pygame.time.Clock()
+
 from textures import *
 
 # Scores
@@ -5,7 +18,6 @@ money = 20
 health = 100
 
 # Tower Costs
-
 Tower_1 = 5
 Tower_2 = 10
 Tower_3 = 25
@@ -17,16 +29,17 @@ RED = (255, 0, 0, 255)
 YELLOW = (255, 255, 0, 255)
 GREEN = (100, 255, 100, 255)
 BROWN = (124, 66, 0, 255)
-
-#Background colour
-BACKGROUND_COLOR = (107, 140, 255)
+WHITE = (255, 255, 255)
 
 # Game Settings
-WIDTH = 1024
-HEIGHT = 768
+WIDTH = 800
+HEIGHT = 600
 FPS = 60
 TITLE = "Barn Defence"
-BGCOLOR = GREEN
+BGCOLOR = GRASS
+
+MAP = TILEMAP
+
 
 TILESIZE = 32
 GRIDWIDTH = WIDTH / TILESIZE

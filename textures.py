f2b14e34360d3725a064becd86028773b0f6a957
ZacAttak4050 <33821116+ZacAttak4050@users.noreply.github.com>
2018-08-20 10:40:21 +1000

    Delete textures.py

--- textures.py
+++
@@ -1,7 +0,0 @@
-import os, pygame
-
-# textures
-GRASS = pygame.image.load(os.path.join('images/Textures/grass.png'))
-DIRT = pygame.image.load(os.path.join('images/Textures/dirt.png'))
-STONE = pygame.image.load(os.path.join('images/Textures/stone.png'))
-WATER = pygame.image.load(os.path.join('images/Textures/water.png'))

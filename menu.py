ea53f462561e29a117359550457949e960067b7c
ZacAttak4050 <33821116+ZacAttak4050@users.noreply.github.com>
2018-08-20 11:00:56 +1000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,15 @@
 #menu.py
-
-# Menu and Scoreboard Pop-ups
+# Menu pop-up
+menu_data = (
+    'Barn Defense',
+    'Start',
+    'Quit',
+)
+PopupMenu(menu_data)
+for e in pygame.event.get():
+    if e.type == USEREVENT and e.code == 'MENU':
+        print 'menu event: %s.%d: %s' % (e.name,e.item_id,e.text)
+        if (e.name,e.text) == ('Main','Quit'):
+            quit()
+            
+# Scoreboard pop-up

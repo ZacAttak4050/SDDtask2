be3752fc8f48ff0ada8591eda1983c611e7020b4
Sheeplet1 <42196548+Sheeplet1@users.noreply.github.com>
2018-08-30 11:28:43 +1000

    Add files via upload

---
+++ textures.py
@@ -0,0 +1,7 @@
+import os, pygame
+
+# textures
+GRASS = pygame.image.load(os.path.join('images/Textures/grass.png'))
+DIRT = pygame.image.load(os.path.join('images/Textures/dirt.png'))
+STONE = pygame.image.load(os.path.join('images/Textures/stone.png'))
+WATER = pygame.image.load(os.path.join('images/Textures/water.png'))
